ca8be53ccf4679a34857bfd3c6de5ce093167f69
Aldo Olivares <aldo2122@outlook.com>
2018-08-11 17:27:46 -0500

    final program

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,38 @@
 fun main(args:Array<String>){
+    //Este es un comentario y es ignorado por el compilador
 
+    /*
+    * Esto es un comentario
+    * de multiples
+    * lineas
+     */
+
+    println("Hola Mundo Estoy Programando en Kotlin")
+
+    println(2 + 6)
+    println(5 - 3)
+    println(2 * 2)
+    println(4 / 2)
+    println(5 % 2)
+    println(5.0 / 2.0)
+    println((7 + 2) * 5)// 45
+
+    val edad: Int = 35
+
+    println(edad)
+
+    val pi: Double = 3.1415
+
+    println(pi)
+
+    var  numeroVariable: Int = 45
+    numeroVariable = 46_000_000
+
+    println(numeroVariable)
+
+    var manzanas: Int = 4
+
+    manzanas /= 2
+
+    println(manzanas)
 }
