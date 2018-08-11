ee453eb686aff39d4523d01edcfa5b5338ab8768
Aldo Olivares <aldo2122@outlook.com>
2018-08-11 17:35:26 -0500

    reto

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,13 @@
 fun main(args:Array<String>){
-
+   /*
+   * 1. Declara una constante de tipo Int llamada miEdad e igualala a tu edad
+   *
+   * 2. Declara una variable de tipo double llamada edadPromedio. Iniciala
+   *     con tu edad como valor y despues asignale el promedio de tu edad
+   *     con mi edad de 35
+   *
+   * 3. Declara una variable llamada respuesta e iniciala con un valor de 3.
+   *     Sumale 7. Restale 2. Multiplicala por 5. ¿Cual es el resultado?
+   *
+   * */
 }
