85e96e022e04ccb8a670cda31ee9966baa04ba6f
Aldo Olivares <aldo2122@outlook.com>
2018-08-12 00:16:12 -0500

    reto

--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,16 @@
 fun main(args:Array<String>){
+    //1. Crea una constante llamada mapa y asigna un par de 1 y 2
+
+    //2. Crea constantes llamadas 'x' y 'y' y asigna los valores de mapa 1 y 2
+
+    /*
+    * 3. Dime cuales de las siguientes expresiones son validas:
+    *
+    * val character: Character = "Dog"
+    * val character: Char = 'd'
+    * val string: String = "Dog"
+    * val string: String = 'd'
+    * */
+
 
 }
