cc0ebf88a72ae5150cf95ad6030042132713d642
Aldo Olivares <aldo2122@outlook.com>
2018-08-12 00:28:44 -0500

    solucion

--- src/main.kt
+++ src/main.kt
@@ -1,15 +1,18 @@
 fun main(args:Array<String>){
     //1. Crea una constante llamada mapa y asigna un par de 1 y 2
 
+    val mapa = Pair(1, 2)
+
     //2. Crea constantes llamadas 'x' y 'y' y asigna los valores de mapa 1 y 2
 
+    val (x, y) = mapa
     /*
     * 3. Dime cuales de las siguientes expresiones son validas:
     *
-    * val character: Character = "Dog"
-    * val character: Char = 'd'
-    * val string: String = "Dog"
-    * val string: String = 'd'
+    * val character: Character = "Dog" --invalida
+    * val character: Char = 'd' --valida
+    * val string: String = "Dog"-- valida
+    * val string: String = 'd' --invalida
     * */
 
 
